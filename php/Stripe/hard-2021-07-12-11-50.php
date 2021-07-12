dbd57d89484c11854b45733d06c5560e93699e02
PCNhat <phamcongnhat1415@gmail.com>
2021-07-13 00:02:57 +0700

    Stripe

---
+++ php/Stripe/hard-2021-07-12-11-50.php
@@ -0,0 +1,16 @@
+<?php
+	/*
+	Given an array of integers, find the first missing positive integer in linear time and constant space. In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+	For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3	
+	*/
+	$arrayInput = [3, 4, -1, 1];
+	$i = 1;
+	while(1) {
+		if(!in_array($i, $arrayInput)) {
+			print($i);
+			return;			
+		}
+
+		$i++;
+	}
+?>
